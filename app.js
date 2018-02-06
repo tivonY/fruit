ffc7a22422a1a6a4ce648b52f866058e93b0368e
tivon <y_tivon@163.com>
2018-02-06 12:21:32 +0800

    init2

--- app.js
+++ app.js
@@ -3,20 +3,23 @@
 var express = require('express');
 var timeout = require('connect-timeout');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var AV = require('leanengine');
+var expressWs = require('express-ws');
 
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
 require('./cloud');
 
 var app = express();
 
+// 启用 WebSocket 支持，如不需要可去除
+expressWs(app);
+
 // 设置模板引擎
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
 // 设置默认超时时间
 app.use(timeout('15s'));
@@ -24,20 +27,33 @@ app.use(timeout('15s'));
 // 加载云引擎中间件
 app.use(AV.express());
 
+// 强制使用 https
 app.enable('trust proxy');
-// 需要重定向到 HTTPS 可去除下一行的注释。
-// app.use(AV.Cloud.HttpsRedirect());
+app.use(AV.Cloud.HttpsRedirect());
+
+// 加载 cookieSession 以支持 AV.User 的会话状态
+app.use(AV.Cloud.CookieSession({ secret: 'randomString', maxAge: 3600000, fetchUser: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.get('/', function(req, res) {
-  res.render('index', { currentTime: new Date() });
-});
 
 // 可以将一类的路由单独保存在一个文件中
+app.use('/batch-update', require('./routes/batch-update'));
+app.use('/captcha', require('./routes/captcha'));
+app.use('/delay-and-retry', require('./routes/delay-and-retry'));
+app.use('/imagemagick', require('./routes/imagemagick'));
+app.use('/meta', require('./routes/meta'));
 app.use('/todos', require('./routes/todos'));
+app.use('/users', require('./routes/users'));
+app.use('/websocket', require('./routes/websocket'));
+app.use('/wechat', require('./routes/wechat'));
+app.use('/xml', require('./routes/xml'));
+
+app.use('/lean-cache', require('./lean-cache'));
+
+app.get('/', function(req, res) {
+  res.redirect('/todos');
+});
 
 app.use(function(req, res, next) {
   // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
@@ -49,7 +65,7 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, _next) {
   if (req.timedout && req.headers.upgrade === 'websocket') {
     // 忽略 websocket 的超时
     return;
--- routes/todos.js
+++ routes/todos.js
@@ -1,17 +1,33 @@
-'use strict';
-var router = require('express').Router();
+var express = require('express');
+var router = express.Router();
 var AV = require('leanengine');
 
+// `AV.Object.extend` 方法一定要放在全局变量，否则会造成堆栈溢出。
+// 详见： https://leancloud.cn/docs/js_guide.html#对象
 var Todo = AV.Object.extend('Todo');
 
-// 查询 Todo 列表
+/**
+ * 定义路由：获取所有 Todo 列表
+ */
 router.get('/', function(req, res, next) {
+  var status = 0;
+  var errMsg = null;
+  if (req.query) {
+    status = req.query.status || 0;
+    errMsg = req.query.errMsg;
+  }
   var query = new AV.Query(Todo);
-  query.descending('createdAt');
-  query.find().then(function(results) {
+  query.equalTo('status', parseInt(status));
+  query.include('author');
+  query.descending('updatedAt');
+  query.limit(50);
+  query.find({sessionToken: req.sessionToken}).then(function(results) {
     res.render('todos', {
       title: 'TODO 列表',
-      todos: results
+      user: req.currentUser,
+      todos: results,
+      status: status,
+      errMsg: errMsg
     });
   }, function(err) {
     if (err.code === 101) {
@@ -19,21 +35,76 @@ router.get('/', function(req, res, next) {
       // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
       res.render('todos', {
         title: 'TODO 列表',
-        todos: []
+        user: req.currentUser,
+        todos: [],
+        status: status,
+        errMsg: errMsg
       });
     } else {
-      next(err);
+      throw err;
     }
   }).catch(next);
 });
 
-// 新增 Todo 项目
+/**
+ * 定义路由：创建新的 todo
+ */
 router.post('/', function(req, res, next) {
   var content = req.body.content;
   var todo = new Todo();
+  if (req.currentUser) {
+    todo.set('author', req.currentUser);
+
+    // 设置 ACL，可以使该 todo 只允许创建者修改，其他人只读
+    // 更多的 ACL 控制详见： https://leancloud.cn/docs/js_guide.html#其他对象的安全
+    var acl = new AV.ACL(req.currentUser);
+    acl.setPublicReadAccess(true);
+    todo.setACL(acl);
+  }
   todo.set('content', content);
-  todo.save().then(function(todo) {
+  todo.set('status', 0);
+  todo.save(null, {sessionToken: req.sessionToken}).then(function() {
     res.redirect('/todos');
+  }).catch(next);
+});
+
+/**
+ * 定义路由：删除指定 todo
+ */
+router.delete('/:id', function(req, res, next) {
+  var id = req.params.id;
+  var status = req.query.status;
+  var todo = AV.Object.createWithoutData('Todo', id);
+  todo.destroy({sessionToken: req.sessionToken}).then(function() {
+    res.redirect('/todos?status=' + status);
+  }, function(err) {
+    res.redirect('/todos?status=' + status + '&errMsg=' + err.message);
+  }).catch(next);
+});
+
+/**
+ * 定义路由：标记指定 todo 状态为「完成」
+ */
+router.post('/:id/done', function(req, res, next) {
+  var id = req.params.id;
+  var todo = AV.Object.createWithoutData('Todo', id);
+  todo.save({status: 1}, {sessionToken: req.sessionToken}).then(function() {
+    res.redirect('/todos');
+  }, function(err) {
+    res.redirect('/todos?errMsg=' + err.message);
+  }).catch(next);
+});
+
+/**
+ * 定义路由：标记指定 todo 状态为「未完成」
+ */
+router.post('/:id/undone', function(req, res, next) {
+  var id = req.params.id;
+  var todo = AV.Object.createWithoutData('Todo', id);
+  todo.save({status: 1}, {sessionToken: req.sessionToken}).then(function() {
+    res.redirect('/todos?status=1');
+  }, function(err) {
+    res.redirect('/todos?status=1&errMsg=' + err.message);
   }).catch(next);
 });
 
